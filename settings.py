b2f7a5f00ca69dca4db8395c06f3583448a05546
xhh <xhh@xhh.me>
2011-12-20 23:11:27 +0800

    Hide django.request error logs when testing in local

--- settings.py
+++ settings.py
@@ -156,8 +156,8 @@ LOGGING = {
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+            'handlers': ['console'],
+            'level': 'CRITICAL',
             'propagate': True,
         },
         'dotpy': {
